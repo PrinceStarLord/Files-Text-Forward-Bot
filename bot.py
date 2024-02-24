80473b4910a71a039379518f40b3e02716bc87ab
Tushar Padir <86295965+PrinceStarLord@users.noreply.github.com>
2024-02-24 12:02:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from config import *
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-TO_CHANNEL_ID = os.environ.get("TO_CHANNEL_ID", "-1001843660143")
-FROM_CHANNEL = os.environ.get("FROM_CHANNEL", "-1001225782985")
+TO_CHANNEL_ID = -1001843660143
+FROM_CHANNEL = -1001225782985
 
 forwarded_files = set()
 
@@ -15,35 +15,46 @@ forwarded_files = set()
 async def start(client, message):
     await message.reply_text("I'm a Files/Video/Documents - Text Forward bot !!")
 
-@app.on_message(filters.chat(FROM_CHANNEL))
-async def forward_files(client, message):
-    if not message.chat.type == "private":
-        try:
-            print("Received message from channel:", message.chat.id)
-            if message.media:
-                print("Media message received:", message.media)
+# @app.on_message(filters.chat(FROM_CHANNEL))
+# async def forward_files(client, message):
+#     if not message.chat.type == "private":
+#         try:
+#             print("Received message from channel:", message.chat.id)
+#             if message.media:
+#                 print("Media message received:", message.media)
                 
-                if message.media.document or message.media.video:
-                    file_name = message.media.document.file_name if message.media.document else message.media.video.file_name
-                    print("File name:", file_name)
+#                 if message.media.document or message.media.video:
+#                     file_name = message.media.document.file_name if message.media.document else message.media.video.file_name
+#                     print("File name:", file_name)
                     
-                    cleaned_name = re.sub(r'[-_+=?:;\'"{\}\[\]\\\/()]+', ' ', file_name)
-                    cleaned_name = re.sub(r'\.\w+', '', cleaned_name)
+#                     cleaned_name = re.sub(r'[-_+=?:;\'"{\}\[\]\\\/()]+', ' ', file_name)
+#                     cleaned_name = re.sub(r'\.\w+', '', cleaned_name)
 
-                    if cleaned_name not in forwarded_files:
-                        forwarded_files.add(cleaned_name)
-                        caption = f"<b>{cleaned_name}</b>\n\nBy @FSearch2bot"
-                        await app.send_message(TO_CHANNEL_ID, caption, parse_mode='html')
-                        print("Forwarded media message")
-                elif message.media.text:
-                    print("Text message received:", message.media.text)
-                    await app.send_message(TO_CHANNEL_ID, message.media.text, link_preview=False, parse_mode='html')
-                    print("Forwarded text message")
-            else:
-                print("No media or text found in the message")
-        except Exception as e:
-            print(f"Error: {e}")
-            print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
+#                     if cleaned_name not in forwarded_files:
+#                         forwarded_files.add(cleaned_name)
+#                         caption = f"<b>{cleaned_name}</b>\n\nBy @FSearch2bot"
+#                         await app.send_message(TO_CHANNEL_ID, caption, parse_mode='html')
+#                         print("Forwarded media message")
+#                 elif message.media.text:
+#                     print("Text message received:", message.media.text)
+#                     await app.send_message(TO_CHANNEL_ID, message.media.text, link_preview=False, parse_mode='html')
+#                     print("Forwarded text message")
+#             else:
+#                 print("No media or text found in the message")
+#         except Exception as e:
+#             print(f"Error: {e}")
+#             print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
+
+
+@app.on_message(filters.chat(FROM_CHANNEL))
+async def forward_message(client, message):
+    try:
+        # Forward the message to the destination channel
+        await app.forward_messages(chat_id=TO_CHANNEL_ID, from_chat_id=message.chat.id, message_ids=message.message_id)
+        print("Message forwarded successfully.")
+    except Exception as e:
+        print(f"Error forwarding message: {e}")
+
 
 print("Bot has started.")
 app.run()
