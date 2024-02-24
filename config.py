3d753a92d476d92e540f6bdd5ac1aeb3e0598bbe
Tushar Padir <86295965+PrinceStarLord@users.noreply.github.com>
2024-02-24 11:02:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import re, os
+
+id_pattern = re.compile(r'^.\d+$') 
+
+API_ID = os.environ.get("API_ID", "4165961")
+API_HASH = os.environ.get("API_HASH", "38ba6396e513b86e9ed7ea534023a9cc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5618090803:AAGx11ftJUVxfUFgiIUg2hl1kZY7tvtV340")
+TO_CHANNEL_ID = os.environ.get("TO_CHANNEL_ID", "-1001843660143")
+FROM_CHANNEL = os.environ.get("FROM_CHANNEL", "-1001225782985")
