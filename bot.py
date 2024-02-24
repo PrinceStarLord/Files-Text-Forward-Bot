0040778fed63447a3a5c4094ed226dce009d0abf
Tushar Padir <86295965+PrinceStarLord@users.noreply.github.com>
2024-02-24 12:25:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ forwarded_files = set()
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
     await message.reply_text("I'm a Files/Video/Documents - Text Forward bot !!")
+
 @app.on_message(filters.chat(FROM_CHANNEL))
 async def forward_message(client, message):
     try:
@@ -21,13 +22,17 @@ async def forward_message(client, message):
         if not message.chat.type == "private" and not message.poll:
             # Check if the message contains media
             if message.photo:
-                photo = message.photo
-                await app.send_photo(chat_id=TO_CHANNEL_ID, photo=photo.file_id, caption=message.text, disable_notification=True)
+                photo = message.photo[-1]  # Get the largest photo size
+                caption = f"**{message.caption}\nUploaded By : @FSearch2Bot**"
+                await app.send_photo(chat_id=TO_CHANNEL_ID, photo=photo.file_id, caption=caption, disable_notification=True)
             elif message.document or message.web_page:
-                await app.send_document(chat_id=TO_CHANNEL_ID, document=message.document.file_id, caption=message.text, disable_notification=True)
+                file_name = re.sub(r'[^\w\s.-]', '', message.document.file_name)  # Remove special characters
+                file_name = file_name.replace('_', ' ')  # Replace underscores with blank space
+                caption = f"**{message.caption}\nUploaded By : @FSearch2Bot**"
+                await app.send_document(chat_id=TO_CHANNEL_ID, document=message.document.file_id, filename=file_name, caption=caption, disable_notification=True)
             else:
-                # Forward the text message
-                await app.send_message(chat_id=TO_CHANNEL_ID, text=message.text, disable_notification=True)
+                text = f"**{message.text}\nUploaded By : @FSearch2Bot**"
+                await app.send_message(chat_id=TO_CHANNEL_ID, text=text, disable_notification=True, parse_mode="MarkdownV2")
             print("Message forwarded successfully.")
     except Exception as e:
         print(f"Error forwarding message: {e}")
